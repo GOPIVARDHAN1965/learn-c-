0e94535ae8ec987ea7fc7bb558589c8edb27dd0a
Gopivardhan <gopivardhangunta@gmail.com>
2024-01-30 18:12:59 -0500

    arrays

--- d1.cpp
+++ d1.cpp
@@ -1,9 +1,62 @@
-#include <iostream>
+// #include <iostream>
+
+#include<iostream>
 
 using namespace std;
 
-int main(){
-    int x;
-    cin >> x;
-    cout << "Hey " << x;
-}
+// int main(){
+//     int x;
+//     cin >> x;
+//     cout << "Hey " << x;
+//     return 0;
+// }
+
+// int main(){
+//     cout << "Hey this will be interesting \n";
+//     cout << "this is nice";
+//     return 0;
+// }
+
+// int main(){
+//     int x,y;
+//     cin >> x >> y;
+//     cout << "value of x: " << x << "\n" << "and value of y: " << y;
+//     return 0;
+// }
+
+// int main(){
+//     //int, long, long long, double, float.
+//     int x = 10;
+//     return 0;
+// }
+
+// int main(){
+//     // string and getline
+//     string s;
+//     getline(cin, s);
+//     cout << s;
+//     return 0;
+// }
+
+// write a program that takes age and says if ur are an adult or no
+// int main(){
+//     int x;
+//     cin >> x;
+//     if (x>=18) {cout << "You are an adult";}
+//     else {cout <<"you are still a kid";}
+// }
+
+// school grading
+// int main(){
+//     int x;
+//     cin >> x;
+//     if(x>=80){cout << "A grade";}
+//     else if (x>=60 && x<=79){cout << "B grade";}
+//     else if (x>=50 && x<=59){cout << "C grade";}
+//     else if (x>=45 && x<=49){cout << "D grade";}
+//     else if (x>=25 && x<=44){cout << "E grade";}
+//     else {cout << "F grade";}
+// }
+
+// ARRAYS
+// int arr[5];
