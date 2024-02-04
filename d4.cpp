c3036a547a69b01b763b75b726e0b920567fa80e
Gopivardhan <gopivardhangunta@gmail.com>
2024-02-04 13:52:22 -0500

    d4 update-d5 start

--- d4.cpp
+++ d4.cpp
@@ -139,7 +139,33 @@ void p16(){
     }
 }
 
+void p17(){
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        char c = 'A';
+        for(int j=0; j<n-i-1; j++)
+        {
+            cout << " ";
+        }
+        for(int j=0; j<2*i+1;j++)
+        {
+            cout << c;
+            if(j<i){c+=1;}
+            else {c-=1;}
+            
+        }
+        for(int j=0; j<n-i-1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+
 int main(){
-    p16();
+    p17();
     return 0;
 }
